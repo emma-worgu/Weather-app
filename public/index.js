5166f324750b7c12d0f28e2d7178ae52eef291a2
Emmanuel <emmanuelworgu@gmail.com>
2020-09-24 15:24:54 +0100

    added location

--- public/index.js
+++ public/index.js
@@ -14,10 +14,39 @@ var key = '56f6937a1d052f6ce1e005bc291d24b3';
 //    console.log(data);
 //   })
 // .catch((err) => {console.log(err)})
+(function getLocation() {
+  if (navigator.geolocation) {
+    console.log('Geolocation Supported');
+    var location = navigator.geolocation.watchPosition((position, err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const lon = position.coords.longitude;
+        const lat = position.coords.latitude;
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`)
+        .then((res) => {return res.json()})
+         .then((data) => {
+           document.getElementById('test').innerHTML = data.weather[0].main;
+           document.getElementById('des').innerHTML = data.main.temp;
+           //console.log(data);
+         })
+       .catch((err) => {
+         document.getElementById('test').innerHTML = 'Something went wrong, Try a well known city or place. If problem persist Contact me'
+         document.getElementById('des').innerHTML = 0.00;
+       });
+      };
+    });
+    console.log(location);
+  } else {
+    console.log('Please Use a Browser that supports Geolocation');
+  }
+}());
 
 
 async function searchWeather() {  
   const searchInput = document.getElementById("search").value
+  const storage = localStorage.setItem('local', searchInput);
+  const output = 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${key}`)
    .then((res) => {return res.json()})
     .then((data) => {
--- public/service_worker.js
+++ public/service_worker.js
@@ -3,7 +3,11 @@ const cacheName = 'v2';
 const cacheAsset = [
   './index.css',
   './index.html',
-  '../asset'
+  '/assest/',
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"',
+  // 'https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"',
+  'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"',
+  // 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"'
 ]
 
 
@@ -19,33 +23,39 @@ self.addEventListener('install', (e) => {
         console.log('Service worker: Caching files');
         cache.addAll(cacheAsset);
       })
-      .then(() => self.skipWaiting())
-  )
+    )
 });
 
 // Activate Event
 self.addEventListener('activate', (e) => {
   console.log('service worker Activated')
-  // Remove unwanted caches
-  e.waitUntil(
-    caches.keys()
-      .then(cacheName => {
-        return Promise.all(
-          cacheName.map(cache => {
-            if (cache !== cacheName) {
-              console.log('Service worker: Clearing old cache');
-              return caches.delete(cache);
-            }
-          })
-        )
-      })
-    )
+//   // Remove unwanted caches
+//   e.waitUntil(
+//     caches.keys()
+//       .then(cacheName => {
+//         return Promise.all(
+//           cacheName.map(cache => {
+//             if (cache !== cacheName) {
+//               console.log('Service worker: Clearing old cache');
+//               return caches.delete(cache);
+//             }
+//           })
+//         )
+//       })
+//     )
 });
 
 // Call Fetch Event
 self.addEventListener('fetch', e => {
   console.log('service working: fetching....');
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
-});
+   caches.match(e.request)
+    .then((res) => {
+        if (res) {
+          console.log(res);
+            return res;
+        } else {
+            return fetch(e.request);
+        }
+    })
+  )}); 
